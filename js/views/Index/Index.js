b5e21a983deecfc3ef109c0bb55da919606b7a15
“gaowei1012” <mingwei2222@126.com>
2020-08-13 10:02:55 +0800

    增加顶部通知 设置

--- js/views/Index/Index.js
+++ js/views/Index/Index.js
@@ -1,13 +1,50 @@
 import React from 'react'
-import { View, Text, StyleSheet, SafeAreaView, ScrollView, Image } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native'
 import { px2dp } from '../../utils/px2dp'
+import TopNavigationBar from '../../common/TopNavigationBar'
+
+// Svg
+import Tips from '../../assets/svg/tips.svg'
+import Setting from '../../assets/svg/setting.svg'
 
 class Index extends React.PureComponent {
    
     render() {
-        
+        const _rightBtn = (
+            <View style={styles.rightBox}>
+                <TouchableOpacity
+                    style={styles.tipsBox}
+                    activeOpacity={1}
+                    onPress={this._tips}
+                >
+                    <Tips width={22} height={22}/>
+                    {/* <Text style={styles.rightText}>通知</Text> */}
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.settingBox}
+                    activeOpacity={1}
+                    onPress={this._setting}
+                >
+                    <Setting width={22} height={22}/>
+                    {/* <Text style={styles.rightText}>设置</Text> */}
+                </TouchableOpacity>
+            </View>
+        )
+        const StatusBar = {
+            backgroundColor: "#ffffff",
+            barStyle: "dark-content",
+        };
+        const renderTop = (
+            <TopNavigationBar
+                title='移动办公'
+                statusBar={StatusBar}
+                style={{ backgroundColor: "#fff" }}
+                rightButton={_rightBtn}
+            />
+        );
         return (
             <SafeAreaView style={styles.landContainer}>
+                {renderTop}
                 <Text>首页</Text>
             </SafeAreaView>
         )
@@ -19,5 +56,15 @@ export default Index
 const styles = StyleSheet.create({
     indexContainer: {
         flex: 1
+    },
+    rightBox: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    tipsBox: {
+
+    },
+    settingBox: {
+        marginLeft: px2dp(6)
     },
 })
