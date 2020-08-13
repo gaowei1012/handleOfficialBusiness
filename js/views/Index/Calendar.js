9a5c8ebbd13f3e155480efbe32b312d410b242f6
“gaowei1012” <mingwei2222@126.com>
2020-08-13 16:05:06 +0800

    增加日历模块

--- js/views/Index/Calendar.js
+++ js/views/Index/Calendar.js
@@ -3,8 +3,22 @@ import { SafeAreaView, Text, View, StyleSheet, TouchableOpacity } from 'react-na
 import TopNavigationBar from '../../common/TopNavigationBar'
 import { GoBack } from '../../utils/GoBack'
 import { px2dp } from '../../utils/px2dp'
+import { Calendar, LocaleConfig } from 'react-native-calendars'
 
-class Calendar extends React.PureComponent {
+
+// 日历组件 中文替换
+LocaleConfig.locales['fr'] = {
+    monthNames: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
+    dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+    dayNamesShort: ['周天', '周一', '周二', '周三', '周四', '周五', '周六']
+};
+LocaleConfig.defaultLocale = 'fr';
+
+// 日历组件 标记点的颜色
+const vacation = { key: 'vacation', color: '#33D9B5', selectedDotColor: '#33D9B5' };
+
+class CalendarPage extends React.PureComponent {
     render() {
         const StatusBar = {
             backgroundColor: "#ffffff",
@@ -21,13 +35,23 @@ class Calendar extends React.PureComponent {
         return (
             <SafeAreaView style={styles.calendarBox}>
                 {renderTop}
-                <Text>出差申请</Text>
+                <Calendar
+                    monthFormat={' yyyy MM '}
+                    markedDates={{
+                        '2019-03-10': { selected: true, selectedColor: '#FFC1B5' },
+                        '2019-03-11': { dots: [vacation], selected: true, selectedColor: '#FFC1B5' },
+                        '2019-03-12': { dots: [vacation] },
+                        '2019-03-13': { dots: [vacation] },
+                    }}
+                    markingType={'multi-dot'}
+
+                />
             </SafeAreaView>
         )
     }
 }
 
-export default Calendar
+export default CalendarPage
 
 const styles = StyleSheet.create({
     calendarBox: {
--- js/views/Index/ClockIn.js
+++ js/views/Index/ClockIn.js
@@ -3,8 +3,16 @@ import {SafeAreaView, Text, View, StyleSheet, TouchableOpacity} from 'react-nati
 import {px2dp} from '../../utils/px2dp'
 import TopNavigationBar from '../../common/TopNavigationBar'
 import { GoBack } from '../../utils/GoBack'
+import moment from 'moment'
 
 class ClockIn extends React.PureComponent {
+    state = {
+        date: null
+    }
+    componentDidMount() {
+        let date = moment().format('HH:ss')
+        this.setState({date})
+    }
     render() {
         const StatusBar = {
             backgroundColor: "#ffffff",
@@ -18,9 +26,18 @@ class ClockIn extends React.PureComponent {
                 leftButton={GoBack(this.props)}
             />
         );
+        const _content = (
+            <View style={styles.contentBox}>
+                <View style={styles.crlesBox}>
+                    <Text>{this.state.date}</Text>
+                    <Text>上班打开</Text>
+                </View>
+            </View>
+        );
         return (
             <SafeAreaView style={styles.clockBox}>
                 {renderTop}
+                {_content}
                 <Text>打卡</Text>
             </SafeAreaView>
         )
@@ -32,5 +49,21 @@ export default ClockIn
 const styles = StyleSheet.create({
     clockBox: {
         flex: 1
+    },
+    contentBox: {
+        width: px2dp(345),
+        alignSelf: 'center',
+        alignItems: 'center',
+        marginTop: px2dp(60)
+    },
+    crlesBox: {
+        width: px2dp(100),
+        height: px2dp(100),
+        borderRadius: px2dp(50),
+        borderWidth: px2dp(6),
+        borderColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: px2dp(20)
     }
 })
